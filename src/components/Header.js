f90a3f8a20ee44116c38d7de74995f87ef2a4a9a
yuyaamano <nba.chako.nba@icloud.com>
2020-10-04 20:26:29 +0900

    ヘッダーコンポーネント仮作成

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Navbar, Nav, Button, Form, FormControl } from "react-bootstrap";
+
+class Header extends Component {
+  render() {
+    return (
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar>
+        <br />
+      </>
+    );
+  }
+}
+export default Header;
