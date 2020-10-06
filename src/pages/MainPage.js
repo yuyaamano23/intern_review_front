f0134a0447c845851d6b93938491b1df57fe45d1
yuyaamano <nba.chako.nba@icloud.com>
2020-10-06 16:39:41 +0900

    スタイル変更

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
-import CompanyCard from "../components/CompanyCard";
+import CompanyCardList from "../components/CompanyCardList";
 import Pagination from "../components/Pagination";
 
 class MainPage extends Component {
@@ -10,12 +10,7 @@ class MainPage extends Component {
         <Header />
         <div className="container text-center">
           <div className="row">
-            <CompanyCard />
-            <CompanyCard />
-            <CompanyCard />
-            <CompanyCard />
-            <CompanyCard />
-            <CompanyCard />
+            <CompanyCardList />
           </div>
         </div>
         <Pagination />
