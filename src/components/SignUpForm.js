9c42a093ceeb445c2916f7a8a6f7aff5d468e057
yuyaamano <nba.chako.nba@icloud.com>
2020-10-08 00:49:26 +0900

    新規登録のバリデーション

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,7 +1,60 @@
 import React, { Component } from "react";
+import Validation from "../validation";
 
 class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      info: {
+        email: "",
+        password: "",
+      },
+      message: {
+        email: "",
+        password: "",
+      },
+      loading: false,
+    };
+  }
+  handleChange = (event) => {
+    const key = event.target.name;
+    const value = event.target.value;
+    const { info, message } = this.state;
+
+    this.setState({
+      info: { ...info, [key]: value },
+    });
+    this.setState({
+      message: {
+        ...message,
+        [key]: Validation.formValidate(key, value),
+      },
+    });
+  };
+
+  canSubmit = () => {
+    const { info, message, loading } = this.state;
+
+    const validInfo =
+      Object.values(info).filter((value) => {
+        return value === "";
+      }).length === 0;
+    const validMessage =
+      Object.values(message).filter((value) => {
+        return value !== "";
+      }).length === 0;
+    return validInfo && validMessage && !loading;
+  };
+
+  // 連打されるのを防ぐ
+  submit = () => {
+    this.setState({ loading: true });
+    this.setState({ loading: false });
+  };
+
   render() {
+    const { info, message } = this.state;
+    console.log(`this.canSubmit=${this.canSubmit()}`);
     return (
       <form
         className="bg-white border rounded container mt-4"
@@ -14,11 +67,17 @@ class SignUpForm extends Component {
           <label htmlFor="exampleInputEmail1">Email address</label>
           <input
             type="email"
+            name="email"
             className="form-control"
             id="exampleInputEmail1"
-            aria-describedby="emailHelp"
+            // aria-describedby="emailHelp"
             placeholder="Enter email"
+            value={info.email}
+            onChange={(event) => this.handleChange(event)}
           />
+          {message.email && (
+            <p style={{ color: "red", fontSize: 8 }}>{message.email}</p>
+          )}
           <small id="emailHelp" className="form-text text-muted">
             公開せませんので安心して入力しやがれ
           </small>
@@ -27,10 +86,16 @@ class SignUpForm extends Component {
           <label htmlFor="exampleInputPassword1">Password</label>
           <input
             type="password"
+            name="password"
             className="form-control"
             id="exampleInputPassword1"
             placeholder="Password"
+            value={info.password}
+            onChange={(event) => this.handleChange(event)}
           />
+          {message.password && (
+            <p style={{ color: "red", fontSize: 8 }}>{message.password}</p>
+          )}
         </div>
         <div className="form-group">
           <label htmlFor="exampleInputPassword1">Confirm Password</label>
@@ -45,6 +110,8 @@ class SignUpForm extends Component {
           type="submit"
           className="btn btn-success"
           style={{ margin: "10px" }}
+          disabled={!this.canSubmit()}
+          onClick={() => this.submit()}
         >
           登録する
         </button>
